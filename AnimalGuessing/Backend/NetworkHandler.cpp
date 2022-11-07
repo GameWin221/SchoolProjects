ab62c7797edb7281eb127aa08d700534824a7308
Mateusz Antkiewicz <mateusz.gamewin1@gmail.com>
2022-11-07 22:12:56 +0100

    Restbed compilation

--- AnimalGuessing/Backend/NetworkHandler.cpp
+++ AnimalGuessing/Backend/NetworkHandler.cpp
@@ -1,1 +1,28 @@
 #include "NetworkHandler.hpp"
+
+NetworkHandler::NetworkHandler()
+{
+    m_Settings = std::make_shared<restbed::Settings>();
+    m_Settings->set_port(8080);
+    m_Settings->set_default_header("Connection", "close");
+    m_Settings->set_default_header( "Access-Control-Allow-Origin", "*");
+}
+
+float NetworkHandler::Calculate(float num1, float num2, std::string operation)
+{
+
+}
+
+std::tuple<float, float, std::string> NetworkHandler::GetPathParameters(const std::shared_ptr<restbed::Session> session) const
+{
+
+}
+std::string NetworkHandler::ToJson(float result)
+{
+
+}
+
+void NetworkHandler::GetHandler(const std::shared_ptr<restbed::Session> session)
+{
+
+}
--- AnimalGuessing/Backend/NetworkHandler.hpp
+++ AnimalGuessing/Backend/NetworkHandler.hpp
@@ -1,7 +1,26 @@
 #pragma once
 
+#include <memory>
+#include <string>
+#include "resource.hpp"
+#include "settings.hpp"
 
-class Network
-Handler
+class NetworkHandler
 {
+public:
+    NetworkHandler();
+    
+    std::shared_ptr<restbed::Resource> GetResource() const { return m_Resource; };
+    std::shared_ptr<restbed::Settings> GetSettings() const { return m_Settings; };
+
+private:
+    float Calculate(float num1, float num2, std::string operation);
+
+    std::tuple<float, float, std::string> GetPathParameters(const std::shared_ptr<restbed::Session> session) const;
+    std::string ToJson(float result);
+
+    void GetHandler(const std::shared_ptr<restbed::Session> session);
+
+    std::shared_ptr<restbed::Resource> m_Resource;
+    std::shared_ptr<restbed::Settings> m_Settings;
 };
